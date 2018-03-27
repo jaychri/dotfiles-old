408e76a49fc78ab433dc536250647690e254e709
jaychri <jaychri@users.noreply.github.com>
2018-03-27 15:42:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ rm ~/.bash_wrappers
 rm ~/.vimrc
 rm ~/.screenrc
 rm ~/.tmux.conf
+rm ~/.dircolors
 ln -s dotfiles/.bashrc ~/
 ln -s dotfiles/.bash_aliases ~/
 ln -s dotfiles/.bash_exports ~/
 ln -s dotfiles/.vimrc ~/
 ln -s dotfiles/.screenrc ~/
 ln -s dotfiles/.tmux.conf ~/
+ln -s dotfiles/.dircolors ~/
