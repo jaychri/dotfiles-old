cce933f8fb2c26afa728e0e4a50ff8c085a3b4f6
Jay Christopherson <Jay.Christopherson@Emerson.com>
2019-02-05 11:34:03 -0600

    Moved link commands under the related remove.

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-rm ~/.bashrc
-rm ~/.bash_aliases
-rm ~/.bash_exports
-rm ~/.bash_wrappers
-rm ~/.vimrc
-rm ~/.screenrc
-rm ~/.tmux.conf
-rm ~/.dircolors
+rm           ~/.bashrc
 ln -s dotfiles/.bashrc ~/
+
+rm           ~/.bash_aliases
 ln -s dotfiles/.bash_aliases ~/
+
+rm           ~/.bash_exports
 ln -s dotfiles/.bash_exports ~/
+
+rm           ~/.bash_wrappers
 ln -s dotfiles/.bash_wrappers ~/
+
+rm           ~/.vimrc
 ln -s dotfiles/.vimrc ~/
+
+rm           ~/.screenrc
 ln -s dotfiles/.screenrc ~/
+
+rm           ~/.tmux.conf
 ln -s dotfiles/.tmux.conf ~/
+
+rm           ~/.dircolors
 ln -s dotfiles/.dircolors ~/
+
