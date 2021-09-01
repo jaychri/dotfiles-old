fd796b3b4116809368fc239ba2ce117983411f36
Jay Christopherson <jay.christopherson@gmail.com>
2021-09-01 02:11:45 -0500

    fixed unlink.sh to have correct path

--- unlink.sh
+++ unlink.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 rm           ~/.bashrc
-cp  dotfiles.old/.bashrc ~/
+cp  .bashrc ~/
 
 rm           ~/.bash_aliases
-cp  dotfiles.old/.bash_aliases ~/
+cp  .bash_aliases ~/
 
 rm           ~/.bash_exports
-cp  dotfiles.old/.bash_exports ~/
+cp  .bash_exports ~/
 
 rm           ~/.bash_wrappers
-cp  dotfiles.old/.bash_wrappers ~/
+cp  .bash_wrappers ~/
 
 rm           ~/.vimrc
-cp  dotfiles.old/.vimrc ~/
+cp  .vimrc ~/
 
 rm           ~/.screenrc
-cp  dotfiles.old/.screenrc ~/
+cp  .screenrc ~/
 
 rm           ~/.tmux.conf
-cp  dotfiles.old/.tmux.conf ~/
+cp  .tmux.conf ~/
 
 rm           ~/.dircolors
-cp  dotfiles.old/.dircolors ~/
+cp  .dircolors ~/
 
 rm           ~/.bash_logout
-cp  dotfiles.old/.bash_logout ~/
+cp  .bash_logout ~/
